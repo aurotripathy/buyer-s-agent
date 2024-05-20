10e6573fe64fe7f36b4eee6b6962829e4bcbe5a7
Auro Tripathy <ipserv@yahoo.com>
2024-05-19 22:15:31 -0700

    hide serpaer api keys

--- random-tests/langchain-serpapi-test.py
+++ random-tests/langchain-serpapi-test.py
@@ -3,9 +3,10 @@ get keys from server.dev (and on other place)
 """
 import os
 import pprint
+from dotenv import load_dotenv
 
-# os.environ["SERPER_API_KEY"] = "a2470c4bd9f80493bb2c3c2fc5bbd01873e0a429df4d4081ff1ec161fcb0ed1e"
-os.environ["SERPER_API_KEY"] = "f82a9006ece734254185cccc35ba0bbacaaca8f1"
+load_dotenv()
+print(f'serper api key: {os.environ["SERPER_API_KEY"]}')
 
 from langchain_community.utilities import GoogleSerperAPIWrapper
 
