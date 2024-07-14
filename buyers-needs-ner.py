d38adc958f60ab41a6dec945ad016a7205e5da2a
Auro Tripathy <ipserv@yahoo.com>
2024-07-13 18:07:44 -0700

    init

--- buyers-needs-ner.py
+++ buyers-needs-ner.py
@@ -16,7 +16,7 @@ print(os.environ["OPENAI_API_KEY"])
 import marvin
 marvin.settings.openai.api_key = os.environ["OPENAI_API_KEY"]
 
-text = """I'm looking for a three bedroom, two bath house in the 95035 zipcode minimum sqaure footage of 2000sqft."""
+text = """I'm looking for a three bedroom, two bath house in the 95035 zipcode, minimum sqaure footage of 2000sqft. and maximum 2500sqft"""
 
 marvin.settings.openai.chat.completions.model = "gpt-3.5-turbo"
 
@@ -33,3 +33,6 @@ entities = marvin.extract(
 )
 
 print(entities)
+
+# output
+# [Entity(name='3', label='BEDROOM'), Entity(name='2', label='BATHROOM'), Entity(name='95035', label='ZIPCODE'), Entity(name='2000-2500', label='HOME_SIZE')]
