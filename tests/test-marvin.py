14d233a2d17657eab6bb0d79d1abfe9f723816bc
Auro Tripathy <ipserv@yahoo.com>
2024-07-21 12:56:23 -0700

    init

---
+++ tests/test-marvin.py
@@ -0,0 +1,35 @@
+"""
+Taking guidance from...https://simmering.dev/blog/structured_output/
+Of all the libraries, marvin appears to be the simplest and works with OpenAI;
+The outlines library, another choice, doesn't work with OpenAI...yet
+"""
+
+from typing import Literal
+from pydantic import BaseModel
+import marvin
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # take environment variables from .env.
+print(os.environ["OPENAI_API_KEY"])
+
+import marvin
+marvin.settings.openai.api_key = os.environ["OPENAI_API_KEY"]
+
+text = """I'm looking for a three bedroom, two bath house in the 95035 zipcode minimum sqaure footage of 2000sqft."""
+
+marvin.settings.openai.chat.completions.model = "gpt-3.5-turbo"
+
+
+class Entity(BaseModel):
+    name: str
+    label: Literal["BEDROOM", "BATHROOM", "ZIPCODE", "HOME_SIZE", "PET_FRIENDLY"]
+
+
+entities = marvin.extract(
+    text,
+    target=Entity,
+    model_kwargs={"temperature": 0.0},
+)
+
+print(entities)
